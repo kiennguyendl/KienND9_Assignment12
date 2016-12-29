57db545e6f59236ab7026f3a3bbc5458c9ff37a4
Kien Nguyen Dang <kiennguyendang@Kiens-Mac.local>
2016-12-29 14:56:53 +0700

    first commit

---
+++ KienND9_Assignment12/Book.swift
@@ -0,0 +1,9 @@
+//
+//  Book.swift
+//  KienND9_Assignment12
+//
+//  Created by Kien Nguyen Dang on 11/28/16.
+//  Copyright © 2016 Kien Nguyen Dang. All rights reserved.
+//
+
+import Foundation
---
+++ KienND9_Assignment12/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  KienND9_Assignment12
+//
+//  Created by Kien Nguyen Dang on 11/28/16.
+//  Copyright © 2016 Kien Nguyen Dang. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
